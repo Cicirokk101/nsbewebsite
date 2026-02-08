80d2bb6e99777a173b0fdb9d3329241edf014390
Cicirokk101 <nykaelaburks@gmail.com>
2026-02-08 08:54:34 -0600

    Update UI components, sponsors page, and add new sponsor images

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="relative z-50">
+    <nav className="fixed top-0 left-0 right-0 z-50">
       <div className="w-full bg-black/90 backdrop-blur-sm">
         <div className="w-full px-4">
           <div className="flex justify-between items-center h-16">
@@ -30,7 +30,7 @@ const Navbar = () => {
               <Image
                 src={'/img/nsbe.png'}
                 alt="NSBE Logo"
-                className="h-12 w-auto"
+                className="h-12 w-auto object-contain"
                 height={48}
                 width={48}
                 priority
